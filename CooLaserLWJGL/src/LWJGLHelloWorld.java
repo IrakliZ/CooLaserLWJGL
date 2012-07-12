03aaad97723a105a908220fbfcf3a65e405f4f69
joshgrant <josh_grant@me.com>
2012-07-12 17:20:38 -0600

    Update with window and run loop

---
+++ CooLaserLWJGL/src/LWJGLHelloWorld.java
@@ -0,0 +1,32 @@
+import org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+
+public class LWJGLHelloWorld {
+	
+	public LWJGLHelloWorld(){
+		try{
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("Hello, LWJGL");
+			Display.create();
+		}catch(LWJGLException e){
+			e.printStackTrace();
+		}
+		
+		//Initialization OpenGL
+		while(!Display.isCloseRequested()){
+			Display.update();
+			Display.sync(60);
+		}
+		
+		Display.destroy();
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+		new LWJGLHelloWorld();
+
+	}
+
+}
