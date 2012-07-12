9b94978b83595393fe5f0e995e77c7184df5ab81
joshgrant <josh_grant@me.com>
2012-07-12 15:51:12 -0600

    Updated with LWJGL

---
+++ CooLaserLWJGL/src/main.java
@@ -0,0 +1,19 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try{
+			Display.create();
+		}catch(LWJGLException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
